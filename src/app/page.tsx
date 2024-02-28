5baf984e10a672cf638c4b79da34167756f90a18
leozhao0709 <leo.zhao.real@gmail.com>
2024-02-27 18:48:52 -0800

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,9 +24,9 @@ export default async function Home() {
 }
 
 // https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config
-export const dynamic = 'auto';
-export const dynamicParams = true;
-export const revalidate = 60;
-export const fetchCache = 'auto';
-export const runtime = 'nodejs';
-export const preferredRegion = 'auto';
+// export const dynamic = 'auto';
+// export const dynamicParams = true;
+// export const revalidate = 60;
+// export const fetchCache = 'auto';
+// export const runtime = 'edge';
+// export const preferredRegion = 'auto';
